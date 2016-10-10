d141cf7aaf375a6798eb76005f9c1ffa77046af6
ISaac Jose Bueno <ibueno21@gmail.com>
2016-10-10 13:35:36 -0300

    Seto  o metodo de camada do calculo no botao

--- app/src/main/java/br/com/javamedicalhealth/conversordesolucoes/FragmentBotao.java
+++ app/src/main/java/br/com/javamedicalhealth/conversordesolucoes/FragmentBotao.java
@@ -6,16 +6,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by isaac on 06/10/16.
  */
 
 public class FragmentBotao extends Fragment {
+
+    Button btn;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_botao,container, false);
         return view;
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        btn = (Button)getActivity().findViewById(R.id.cmdCalcular);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cacular(view);
+            }
+        });
+
+    }
+
+    public void cacular(View view){
+        //apago os fragmentos
+        ((MainActivity)getActivity()).apagaFragments();
+        //chamo metodo de criação do fragmento de resultado
+        ((MainActivity)getActivity()).chamaResultado();
+    }
+
 }
