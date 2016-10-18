577c8c4f6a6f2e44ac58befc6c6fa65ddff729f6
Isaac Jose Bueno (notebook) <ibueno21@gmail.com>
2016-10-18 01:16:32 -0200

    dentro do metodo que salva estado do fragmento seto o valor do modelo dentro do maindactivit para poder retornar ele semrpe atualizado

--- app/src/main/java/br/com/javamedicalhealth/conversordesolucoes/FragmentExistente.java
+++ app/src/main/java/br/com/javamedicalhealth/conversordesolucoes/FragmentExistente.java
@@ -91,6 +91,8 @@ public class FragmentExistente extends Fragment {
         outState.putSerializable("txtPorcento", txtPorcento.getText().toString());
         outState.putSerializable("txtVolume", txtVolume.getText().toString());
         outState.putSerializable("spnTipo", spnTipo.getSelectedItemPosition());
+        MainActivity mainActivity = (MainActivity)getActivity();
+        mainActivity.setModelSolucao(modelSolucao);
         super.onSaveInstanceState(outState);
     }
 }
