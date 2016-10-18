bcd4e481507d193a75dcdbc4e208a95e8eb84819
Isaac Jose Bueno (notebook) <ibueno21@gmail.com>
2016-10-18 01:10:43 -0200

    crio o override do spinner para pegar qual item foi selecionado

--- app/src/main/java/br/com/javamedicalhealth/conversordesolucoes/FragmentPrescricao.java
+++ app/src/main/java/br/com/javamedicalhealth/conversordesolucoes/FragmentPrescricao.java
@@ -81,6 +81,17 @@ public class FragmentPrescricao extends Fragment {
             }
         });
 
+        spnTipo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                modelSolucao.setTipoAmpola(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     @Override
