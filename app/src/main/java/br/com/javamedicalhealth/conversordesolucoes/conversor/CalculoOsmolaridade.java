9d17aaccd4bae791f7c0fe068e0ddbb900505838
Isaac Jose Bueno (notebook) <ibueno21@gmail.com>
2016-10-20 04:28:29 -0200

    criação do modulo de calculo da osmolaridade inserindo valores fixos para NACL e GLICOSE e criação dos metodos de recuperação destes valores e calculo paara a solucao

---
+++ app/src/main/java/br/com/javamedicalhealth/conversordesolucoes/conversor/CalculoOsmolaridade.java
@@ -0,0 +1,29 @@
+package br.com.javamedicalhealth.conversordesolucoes.conversor;
+
+/**
+ * Created by gaijin on 20/10/2016.
+ */
+
+public class CalculoOsmolaridade {
+    private static final Double NaCl = 22.98976928 + 35.45;
+    private static final Double G = (12.011 * 6) + (1.008 * 12) + (15.999 * 6);
+
+
+    public static Double getNaCl() {
+        return NaCl;
+    }
+
+    public static Double getG() {
+        return G;
+    }
+
+
+    public String calculaOsmolaridade(Double porcentagem, String tipo){
+        String t = "";
+        if(tipo == "SF")
+            t = String.valueOf(( (porcentagem / 0.100) *( 1 / NaCl)) * 2 * 1000);
+        else if (tipo == "G")
+            t = String.valueOf(( (porcentagem / 0.100) *( 1 / G)) * 1 * 1000);
+        return  t;
+    }
+}
